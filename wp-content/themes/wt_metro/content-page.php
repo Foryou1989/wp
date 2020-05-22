e7a0e6d2bea91b0542ff6106de8b13f73d8a1bc3
BO <boyce.xiong@d1m.cn>
2020-05-22 10:24:09 +0800

    5.21修改

--- wp-content/themes/wt_metro/content-page.php
+++ wp-content/themes/wt_metro/content-page.php
@@ -51,53 +51,7 @@
 			</div><!-- /author-info -->
 		
 		<?php } //endif; ?>
-		
-		<?php //add social buttons
-		if ( wt_get_option( 'wt_show_page_social' ) == 1 ) { ?>
-		
-		<div class="entry-social">
-			
-			<span class="fb">
-				<div id="fb-root"></div>
-				<script>
-					(function(d, s, id) {
-						var js, fjs = d.getElementsByTagName(s)[0];
-						if (d.getElementById(id)) return;
-						js = d.createElement(s); js.id = id;
-						js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
-						fjs.parentNode.insertBefore(js, fjs);
-					}(document, 'script', 'facebook-jssdk'));
-				</script>
-				<div class="fb-like" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>
-			</span>
-			
-			<span class="twitter">			
-				<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
-				<a href="http://twitter.com/share" class="twitter-share-button"
-					data-url="<?php the_permalink(); ?>"
-					data-text="<?php the_title(); ?>"
-					data-count="horizontal">Tweet
-				</a>
-			</span>
-			
-			<span class="gplus">	
-				<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-				<div class="g-plusone" data-size="medium"></div>
-			</span>
-			
-			<span class="pinterest">
-				<a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&media=<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' ); echo $thumb['0']; ?>&description=<?php the_title(); ?>" class="pin-it-button" count-layout="horizontal">Pin It</a><script type="text/javascript" src="http://assets.pinterest.com/js/pinit.js"></script>
-			</span>
-			
-			<span class="linkedin">	
-				<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
-				<script type="IN/Share" data-counter="right"></script>
-			</span>			
-	  
-		</div><!-- /entry-social -->
-		
-		<?php } ?>
-		
+				
 	</footer><!-- /entry-footer -->
 	<?php if ( wt_get_option( 'wt_show_page_comments' ) == 1 ) { ?>
 		<?php comments_template( '', true ); ?>
